825aa0d85b38f76fffd88dda266ed32aebf93e7a
SamG06 <16375795+SamG06@users.noreply.github.com>
2021-03-03 00:18:48 -0500

    make it better

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fastify.register(require('fastify-cors'), {
 const scraper = require('./scraper.js');
 
 fastify.get('/', function (request, reply) {
-    reply.send({ message: 'Scraper for hacker news whoishiring' })
+    reply.send({ message: 'Scraper for hacker news whoishiring site' })
 })
 
 fastify.route({ method: 'GET', url: '/jobs', handler: scraper })
