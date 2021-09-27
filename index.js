fc07c9fecefbde8f02d5e5ab18741f2b5910491c
SamG06 <16375795+SamG06@users.noreply.github.com>
2021-09-27 15:25:26 -0400

    v1

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const fastify = require('fastify')({
-    logger: true
-})
+const fastify = require("fastify")({
+  logger: true,
+});
 
-fastify.register(require('fastify-cors'), {
-    origin: '*'
-})
+fastify.register(require("fastify-cors"), {
+  origin: "*",
+});
 
-const scraper = require('./scraper.js');
+const scraper = require("./scraper.js");
 
-fastify.get('/', function (request, reply) {
-    reply.send({ message: 'Scraper for hacker news whoishiring' })
-})
+fastify.get("/", function (request, reply) {
+  reply.send({ message: "Scraper for hacker news whoishiring" });
+});
 
-fastify.route({ method: 'GET', url: '/jobs', handler: scraper })
+fastify.route({ method: "GET", url: "/jobs", handler: scraper });
 
-fastify.listen(process.env.PORT, '0.0.0.0', function (err, address) {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-    fastify.log.info(`server listening on ${address}`)
-})
+fastify.listen(3003, "0.0.0.0", function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+});
--- scraper.js
+++ scraper.js
@@ -1,6 +1,14 @@
 const puppeteer = require('puppeteer');
 
-const minutes = 600 * 3000
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+
+const minutes = 600 * 3000;
+
 setInterval(() => {
     console.log('updated job posts', new Date())
     getJobPosts();
@@ -15,7 +23,10 @@ const getJobPosts = async () => {
           '--disable-setuid-sandbox',
         ],
       });
+    try {
     const page = await browser.newPage();
+
+    // Not loading what we don't need from the page
     await page.setRequestInterception(true);
     page.on('request', (request) => {
         if (['image', 'stylesheet', 'font', 'script'].indexOf(request.resourceType()) !== -1) {
@@ -24,9 +35,17 @@ const getJobPosts = async () => {
             request.continue();
         }
     });
-    const ycom = 'https://news.ycombinator.com/'
+
     // The main page with all the listings for whoishiring
-    await page.goto(ycom + 'submitted?id=whoishiring');
+    const ycom = 'https://news.ycombinator.com/';
+
+    try{
+        await page.goto(ycom + 'submitted?id=whoishiring');
+    }
+    catch (e) {
+        console.error('Something went wrong. You can cry now.', e);
+        return;
+    }
 
     // Grab all posts
     const allPosts = await page.evaluate(() => Array.from(document.querySelectorAll('.storylink'), e => {
@@ -35,7 +54,8 @@ const getJobPosts = async () => {
         }
     }));
 
-    const jobPostPageLinks = allPosts.filter(x => x !== null);
+    const jobPostPageLinks = allPosts.filter(post => post !== null);
+
     let pageNum = 1;
     let noMorePages = false;
     let allJobPosts = [];
@@ -44,17 +64,50 @@ const getJobPosts = async () => {
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
+    
     while (noMorePages === false) {
-        await sleep(9000);
-        const mostRecentMonthPage = `${ycom}${jobPostPageLinks[0]}&&p=${pageNum}`;
+        await sleep(9000); // Don't parse too fast and get blocked :3
 
-        console.log(pageNum)
+        const mostRecentMonthPage = `${ycom}${jobPostPageLinks[0]}&&p=${pageNum}`;
+        
+        console.log('Page ' + pageNum);
+        console.log(jobPostPageLinks[0]);
+
         await page.goto(mostRecentMonthPage, 'rm');
 
         const pageJobPosts = await page.evaluate((eval) => {
             const moreLink = document.querySelector('.moreLink');
+            month = document.querySelector('.storylink').innerText.match(/\(([^)]+)\)/)[1];
 
-            const newArray = Array.from(document.querySelectorAll('.comtr'), e => { return { text: e.querySelector('.comment').textContent, id: e.id } });
+    
+            const newArray = Array.from(document.querySelectorAll('.comtr'), e => { 
+                e.querySelector('.reply').remove();
+                // check if message is just a reply through indentation
+                const indentWidth = e.querySelector('.ind img').width;
+                if(indentWidth > 0){
+                    return;
+                }
+                const element = e.querySelector('.comment .commtext');
+                const date_time = e.querySelector('.age').title;
+
+                if(!element){
+                    return;
+                }
+                const jobPostNodes = element.childNodes;
+
+                const title = jobPostNodes[0].textContent;
+           
+                const content =  [...element.querySelectorAll('p')].map(p => `<p>${p.innerHTML}</p>`).join('');
+                   
+                return { 
+                    month,
+                    title,
+                    content,
+                    date_time,
+                    id: e.id,
+                    indentWidth
+                } 
+            });
 
             return { moreLink, newArray };
         })
@@ -70,23 +123,31 @@ const getJobPosts = async () => {
     }
 
 
-    const newJobPackage = [];
-    allJobPosts.forEach(({ text, id }) => {
-        const placement = text.lastIndexOf('|');
-        const data = {
-            title: text.slice(0, placement + 1),
-            content: text.slice(placement + 1, text.length),
-            id
 
+    const newJobPackage = [];
+    
+    allJobPosts.forEach((job) => {
+        const data = job;
+        if(!job){
+            return;
         }
-        if (data.title != '') {
+        if (job.title != '') {
+            data.content = DOMPurify.sanitize(job.content, {USE_PROFILES: {html: true}});
             newJobPackage.push(data);
         }
 
     })
 
+   
     jobPackage = newJobPackage;
+    await browser.close();
+
     return jobPackage
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await browser.close();
+    }
 }
 
 getJobPosts();
